a5dfec2a7b28d434986bab3b3f2e4621ee59b4cd
ralphdaryn <ralphdarync@gmail.com>
2024-10-04 20:19:41 -0400

    add theandclub components

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h3 className="footer__text">footer</h3>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Main/Main.js
@@ -0,0 +1,10 @@
+import './Main.scss';
+
+const Main = () => {
+    return (
+        <div className="main">
+            <h2 className="main__text">Middle</h2>
+        </div>
+    )
+}
+export default Main;
